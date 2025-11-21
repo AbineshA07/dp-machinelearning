bde1684ec68aec98e5b24fed7497d25a0d8a13b0
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 17:25:39 +0530

    Fix typo in variable name for flipper_length_mm

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('Male', 'Female'))
   bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill Depth (mm)', 32.1, 21.5, 17.2)
-  filpper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
+  flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
 
   # Create a DataFrame for the input features
