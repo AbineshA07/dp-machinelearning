540c64bd193094a7727a19694e70f92fd8bc4112
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 18:24:31 +0530

    Update model training with raw input data

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ with st.expander('Data preparation'):
 # Model training and inference
 ## Train the ML model
 clf = RandomForestClassifier()
-clf.fit(X, y)
+clf.fit(X_raw, y)
 
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
