d28c2a8e2dea7873cf97b143243aff3441c7466a
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 16:13:15 +0530

    Fix typo in expander label for data visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ x
 st.write('**y**')
 y = df.species
 y
-with st.expender('Data Visualization'):
+with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g',color = 'species')
