0612eb2a13fc95c64a63c3ed7be0f1ef19281dca
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 13:15:04 +0530

    Add data expander to streamlit app
    Added an expander for displaying data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is App build a Machine Learning Model!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Ramw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
