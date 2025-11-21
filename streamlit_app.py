a3393bc1f1d9579d3991d2b46fd1c2819659cc54
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 13:10:24 +0530

    Import pandas and load dataset for analysis
    Added pandas import and loaded dataset from URL.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info('This is App build a Machine Learning Model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
