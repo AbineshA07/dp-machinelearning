afadbfb411885fefdadc96f81f3f041572ad170a
AbineshA07 <154863663+AbineshA07@users.noreply.github.com>
2025-11-21 17:34:48 +0530

    Add data preparation for encoding categorical variables
    Added data preparation step to encode categorical variables.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,7 @@ with st.expander('Input features'):
   st.write('**Combined penguins data**')
   input_penguins
 
-
+# Data preparation
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
